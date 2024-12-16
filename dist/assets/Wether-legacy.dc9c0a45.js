7703c09b061d5aeba55df180e38b177df5e80181
Long <2190008995@qq.com>
2024-12-16 12:48:42 +0000

    upload v3.41.0 dist files

--- dist/assets/Wether-legacy.dc9c0a45.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy.b89efed1.js"],(function(e){"use strict";var t,n,r;return{setters:[function(e){t=e.d,n=e.c,r=e.cd}],execute:function(){e("W",(function(e){var c=t();return n(r,{get colorScheme(){return e.yes?"success":"danger"},get children(){return c("global.".concat(e.yes?"yes":"no"))}})}))}}}));
